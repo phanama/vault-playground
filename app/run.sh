03a4c767147823a44b9caddae0a8062b42db2f36
Yudi A Phanama <11147376+phanama@users.noreply.github.com>
2020-06-23 15:54:51 +0700

    Update run.sh

--- app/run.sh
+++ app/run.sh
@@ -1,22 +1,14 @@
 #!/usr/bin/env bash
-
 set -eu
-
 export VAULT_ADDR=http://127.0.0.1:8200
 export APP_NAME="$1"
 export APP_PORT="$2"
 DYNAMIC=$3
 
-
 if ! [ "$DYNAMIC" == "true" ]; then
     VAULT_PASSWORD="${APP_NAME}vaultpassword"
-
-    #VAULT_TOKEN=$(curl --silent -XPOST http://$VAULT_ADDRESS/v1/auth/userpass/login/$APP_NAME/ -d '{"password": "'$VAULT_PASSWORD'"}' | jq -r '.auth.client_token') 
-
     vault login -method=userpass username="$APP_NAME" password="${VAULT_PASSWORD}" > /dev/null
-
     SECRET=$(vault read -format=json kv/$APP_NAME/postgres)
-
     export POSTGRES_USERNAME=$(echo $SECRET | jq -r '.data.username')
     export POSTGRES_PASSWORD=$(echo $SECRET | jq -r '.data.password')
     export POSTGRES_HOSTNAME=$(echo $SECRET | jq -r '.data.hostname')
